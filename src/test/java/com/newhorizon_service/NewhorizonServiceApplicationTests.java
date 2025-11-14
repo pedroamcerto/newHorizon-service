f55536a883489c3647982c0aa3a11cf63944bd66
Pedro Certo <113721862+pedroamcerto@users.noreply.github.com>
2025-11-13 23:37:17 -0300

    create: project base

---
+++ src/test/java/com/newhorizon_service/NewhorizonServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.newhorizon_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewhorizonServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
